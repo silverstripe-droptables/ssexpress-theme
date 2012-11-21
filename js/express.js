dcedd67d1204e1f32fb2430a3932e48fdb80e4d8
Robert Curry <robert@silverstripe.com>
2012-11-21 14:43:57 +1300

    FIX Only show carousel controls and add javascript if necessary.

--- js/express.js
+++ js/express.js
@@ -37,23 +37,24 @@
 	    }
 	    /*************************/
 
-	    $('.carousel').carousel({
-			interval: 8000,
-			pause: ""
-	    });  
+		$('.carousel').each(function(){
+			if ($(this).find('.carousel-inner > div').length > 1) {
+				$(this).carousel({
+					interval: 8000,
+					pause: ""
+				});
 
-
-
-	    $('.carousel').on('click','#pause .btn',function(){
-	    	if($(this).closest('.carousel').hasClass('play')){
-	    		$(this).closest('.carousel').carousel('cycle').removeClass('play');
-	    		$(this).find('.icon').removeClass('icon-play-js').html('&#xf04c;');
-	    	}else{
-	    		$(this).closest('.carousel').carousel('pause').addClass('play');
-	    		$(this).find('.icon').addClass('icon-play-js').html('&#xf04b;');
-	    	}	    	
-	    });
-
+				$(this).on('click','#pause .btn',function() {
+					if ($(this).closest('.carousel').hasClass('play')) {
+						$(this).closest('.carousel').carousel('cycle').removeClass('play');
+						$(this).find('.icon').removeClass('icon-play-js').html('&#xf04c;');
+					} else {
+						$(this).closest('.carousel').carousel('pause').addClass('play');
+						$(this).find('.icon').addClass('icon-play-js').html('&#xf04b;');
+					}
+				});
+			}
+		});
 
 
 		$('.navbar a .showChildren').click(function(e){
--- templates/Includes/Carousel.ss
+++ templates/Includes/Carousel.ss
@@ -1,8 +1,7 @@
 <div id="myCarousel" class="carousel slide">
 	<!-- Carousel items -->
 	<div class="carousel-inner">
-		<% loop CarouselItems %>
-			<% if not Archived %>
+		<% loop VisibleCarouselItems %>
 			<div class="<% if First %>active <% end_if %>item">
 				<a<% if Link %> href="$Link.Link"<% end_if %>>
 					$Image.SetRatioSize(670, 503)
@@ -16,11 +15,12 @@
 					<% end_if %>
 				</a>
 			</div>
-			<% end_if %>
 		<% end_loop %>
 	</div>
-	<!-- Carousel nav -->
-	<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-	<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-	<div id="pause"><div class="btn"><span class="icon">&#xf04c;</span></div></div>
+	<% if VisibleCarouselItems && VisibleCarouselItems.Count != 1 %>
+		<!-- Carousel nav -->
+		<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		<div id="pause"><div class="btn"><span class="icon">&#xf04c;</span></div></div>
+	<% end_if %>
 </div>
